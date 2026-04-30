af8d519210d28f2127b115052c310a35ccca952b
pinguuiin <yuping199407@hotmail.com>
2026-05-01 02:27:23 +0300

    Fix trailing handling in tokenization

--- src/CommandParser.cpp
+++ src/CommandParser.cpp
@@ -9,6 +9,8 @@ CommandParser::ParsedCommand CommandParser::parse(const std::string& msg)
 	if (line.empty())
 		return cmd;
 
+	if (msg.empty())
+		return cmd;
 	// Command
 	auto pos = msg.find(' ');
 	// no space found
@@ -20,8 +22,17 @@ CommandParser::ParsedCommand CommandParser::parse(const std::string& msg)
 	cmd.command = msg.substr(0, pos);
 
 	// Params
-	// break the loop when there are only spaces left in the unprocessed message
-	while ((pos = msg.find_first_not_of(' ', pos)) != std::string::npos) {
+	while (true) {
+		pos = msg.find_first_not_of(' ', pos);
+		// break the loop when there are only spaces left in the unprocessed message
+		if (pos == std::string::npos) {
+			return cmd;
+		}
+		// if the next param starts with ':', the rest of the message is the trailing
+		if (msg[pos] == ':') {
+			cmd.trailing = msg.substr(pos + 1);
+			return cmd;
+		}
 		// find next param before space or line end
 		auto end = msg.find(' ', pos);
 		if (end == std::string::npos) {
