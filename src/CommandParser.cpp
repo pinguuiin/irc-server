21bbcd31c911f0a56ec5b8640f81396163885213
pinguuiin <yuping199407@hotmail.com>
2026-05-01 23:59:05 +0300

    minor optimization for tokenizer

--- src/CommandParser.cpp
+++ src/CommandParser.cpp
@@ -9,37 +9,32 @@ CommandParser::ParsedCommand CommandParser::parse(const std::string& msg)
 	if (line.empty())
 		return cmd;
 
-	if (msg.empty())
-		return cmd;
 	// Command
-	auto pos = msg.find(' ');
+	size_t pos = line.find(' ');
 	// no space found
 	if (pos == std::string::npos) {
-		cmd.command = msg;
+		cmd.command = line;
 		return cmd;
 	}
 	// otherwise extract substring before first space encountered as command
-	cmd.command = msg.substr(0, pos);
+	cmd.command = line.substr(0, pos);
 
 	// Params
-	while (true) {
-		pos = msg.find_first_not_of(' ', pos);
-		// break the loop when there are only spaces left in the unprocessed message
-		if (pos == std::string::npos) {
-			return cmd;
-		}
+	// break the loop when there are only spaces left in the unprocessed message
+	while ((pos = line.find_first_not_of(' ', pos)) != std::string::npos) {
+
 		// if the next param starts with ':', the rest of the message is the trailing
-		if (msg[pos] == ':') {
-			cmd.trailing = msg.substr(pos + 1);
+		if (line[pos] == ':') {
+			cmd.trailing = line.substr(pos + 1);
 			return cmd;
 		}
 		// find next param before space or line end
-		auto end = msg.find(' ', pos);
+		size_t end = line.find(' ', pos);
 		if (end == std::string::npos) {
-			cmd.params.push_back(msg.substr(pos));
+			cmd.params.push_back(line.substr(pos));
 			return cmd;
 		}
-		cmd.params.push_back(msg.substr(pos, end - pos));
+		cmd.params.push_back(line.substr(pos, end - pos));
 		pos = end;
 	}
 	return cmd;
